0cb2a7c111228a09d5251127bf322c465ad74d57
Dalton Barreto <daltonmatos@gmail.com>
2016-09-22 15:21:43 -0300

    Stub de alguns testes
    IN-1687

---
+++ tests/filters/requestfilter.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+class RequestFilterTest(TestCase):
+
+  def test_is_app_create(self):
+    """
+    HTTP POST em /v2/apps/
+    """
+    self.fail()
+
+  def test_is_app_update(self):
+    """
+    HTTP PUT em /v2/apps/
+    """
+    self.fail()
+
+  def test_is_app_delete(self):
+    self.fail()
+
+  def test_get_app_ids_v2_apps(self):
+    """
+     Retorna a lista de apps envilvidas em um request a /v2/apps/
+    """
+    self.fail()
+
+  def test_get_app_ids_v2_apps_appid(self):
+    """
+     Retorna a lista de apps envilvidas em um request a /v2/apps/<app-id>
+    """
+    self.fail()
+
+  def test_get_app_ids_v2_groups_1_depth(self):
+    """
+     Retorna a lista de apps envilvidas em um request a /v2/groups/
+    """
+    self.fail()
+
+  def test_get_app_ids_v2_groups_n_depth(self):
+    """
+     Retorna a lista de apps envilvidas em um request a /v2/groups/
+    """
+    self.fail()
+
+  def test_get_app_ids_v2_groups_groupid_1_depth(self):
+    """
+     Retorna a lista de apps envilvidas em um request a /v2/groups/<group-id>
+    """
+    self.fail()
+
+  def test_get_app_ids_v2_groups_groupid_n_depth(self):
+    """
+     Retorna a lista de apps envilvidas em um request a /v2/groups/<group-id>
+    """
+    self.fail()
