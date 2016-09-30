308f6343df36aa8bb14437fc66b80a15a55e8f6f
Dalton Barreto <daltonmatos@gmail.com>
2016-09-30 16:28:55 -0300

    Teste de remoção do Transfer-encoding header pra /v2/*
    IN-1758

--- tests/app.py
+++ tests/app.py
@@ -1,9 +1,21 @@
 
+from mock import patch
 from unittest import TestCase
+import hollowman.upstream
+
+class DummyResponse(object):
+    def __init__(self, headers={}):
+        self.headers = headers
+        self.content = None
+        self.status_code = 200
 
 
 class TestApp(TestCase):
 
   def test_remove_transfer_encoding_header(self):
-    self.fail()
+    with patch('hollowman.upstream.replay_request', return_value=DummyResponse(headers={"Transfer-Encoding": "chunked"})):
+      from hollowman.app import application
+      with application.test_client() as client:
+        response = client.get("/v2/apps")
+        self.assertTrue("Content-Encoding" not in response.headers)
 
