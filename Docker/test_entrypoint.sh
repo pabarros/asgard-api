c077e16b008e89eeea1fe545ba4badac02ebaa56
Miguel E dos Santos <miguel.santos@sieve.com.br>
2017-01-23 11:54:04 -0200

    Added .dockerignore + other fixes

--- Docker/test_entrypoint.sh
+++ Docker/test_entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/env sh
+#!/usr/bin/env sh
 
 {
     apk add --update gcc python-dev musl-dev && \
-    pip install -r requirements-dev.txt && \
+    pip install -r /tmp/requirements-dev.txt && \
     find . -iname *.pyc -delete && \
     py.test --cov=hollowman --cov-report term-missing -v -s
 } || {
-    return 1;
+    exit 1;
 }
