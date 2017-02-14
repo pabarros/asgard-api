da8df5e074c5d001aefd175661aa4485683eafa1
Miguel E dos Santos <miguel.santos@sieve.com.br>
2017-02-14 16:04:18 -0200

    IN-2033

---
+++ hollowman/filters/base_constraint.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+import json
+from hollowman.filters import BaseFilter
+
+
+class BaseConstraintFilter(BaseFilter):
+
+    def __init__(self, ctx):
+        super(BaseConstraintFilter, self).__init__(ctx)
+
+    def run(self, request):
+        data = request.get_json()
+
+        if self.is_single_app(data) and (('constraints' not in data)\
+            or (data['constraints'] == [])):
+            data['constraints'] = [
+                [
+                    "exclusive",
+                    "UNLIKE",
+                    ".*"
+                ]
+            ]
+
+            request.data = json.dumps(data)
+
+        return request
--- hollowman/filters/request.py
+++ hollowman/filters/request.py
@@ -3,6 +3,7 @@
 from hollowman.filters.dns import DNSRequestFilter
 from hollowman.filters.trim import TrimRequestFilter
 from hollowman.filters.default_scale import DefaultScaleRequestFilter
+from hollowman.filters.base_constraint import BaseConstraintFilter
 from hollowman.filters import Context
 from hollowman.filters.forcepull import ForcePullFilter
 from hollowman.conf import marathon_client
@@ -13,6 +14,7 @@ _filters = [
     DNSRequestFilter(_ctx),
     TrimRequestFilter(_ctx),
     ForcePullFilter(_ctx),
+    BaseConstraintFilter(_ctx),
     # Dsligado por enquanto, mais detalhes: https://sievetech.atlassian.net/browse/IN-2035
     #DefaultScaleRequestFilter(_ctx),
 ]
