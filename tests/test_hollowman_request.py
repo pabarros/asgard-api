2a1a821b6b54e0da02326073d47721476709364c
Miguel E dos Santos <miguel.santos@sieve.com.br>
2017-02-14 11:46:05 -0200

    Autopep8 on test_hollowman_request
    IN-2032

--- tests/test_hollowman_request.py
+++ tests/test_hollowman_request.py
@@ -5,14 +5,15 @@ import json
 
 # from flask.wrappers import Request
 
+
 class TestHollowmanRequest(TestCase):
 
     # see: http://flask.pocoo.org/docs/0.12/testing/#other-testing-tricks
     def test_default_cache_disabled(self):
         with application.test_request_context(
-            '/v2/apps/foo',
-            data='{"foo":"bar"}',
-            content_type= "application/json"):
+                '/v2/apps/foo',
+                data='{"foo":"bar"}',
+                content_type="application/json"):
             json1 = request.get_json()
             request.data = '{"abc":123}'
             json2 = request.get_json()
