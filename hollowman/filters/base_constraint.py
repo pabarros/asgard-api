6da063b00ce63646d0183ff6c6c2dc171cb8d5a0
Dalton Barreto <daltonmatos@sieve.com.br>
2017-02-21 10:03:26 -0300

    Fix tests. Now we always fetch the original app.
    IN-2033

--- hollowman/filters/base_constraint.py
+++ hollowman/filters/base_constraint.py
@@ -13,16 +13,20 @@ class BaseConstraintFilter(BaseFilter):
         if request.is_json and request.data:
             data = request.get_json()
 
-            if self.is_single_app(data) and (('constraints' not in data)\
-                or (data['constraints'] == [])):
-                data['constraints'] = [
-                    [
-                        "exclusive",
-                        "UNLIKE",
-                        ".*"
-                    ]
-                ]
+            if self.is_single_app(data):
+                origina_app = self.get_original_app(request)
+                app_dict = json.loads(origina_app.to_json())
 
-                request.data = json.dumps(data)
+                app_dict.update(data)
+                if 'constraints' not in app_dict or app_dict['constraints'] == []:
+                    app_dict['constraints'] = [
+                        [
+                            "exclusive",
+                            "UNLIKE",
+                            ".*"
+                        ]
+                    ]
+
+                request.data = json.dumps(app_dict)
 
         return request
--- tests/filters/test_base_constraint.py
+++ tests/filters/test_base_constraint.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 from tests import RequestStub
 from hollowman.filters.request import _ctx
 
+import mock
 
 class BaseConstraintFilterTest(TestCase):
 
@@ -14,21 +15,29 @@ class BaseConstraintFilterTest(TestCase):
     def test_simple_app(self):
         data = {"id": "/foo/bar"}
         request = RequestStub(data=data)
-        modified_request = self.filter.run(request)
 
-        self.assertEqual(
-            modified_request.get_json(),
-            {
-                "id": "/foo/bar",
-                "constraints": [
-                    [
-                        "exclusive",
-                        "UNLIKE",
-                        ".*"
+        from marathon.models.app import MarathonApp
+        with mock.patch.object(self.filter, "ctx") as ctx_mock:
+            request = RequestStub(path="/v2/apps//app/foo", data=data, method="PUT")
+            ctx_mock.marathon_client.get_app.return_value = MarathonApp(**data)
+
+            filtered_request = self.filter.run(request)
+            ctx_mock.marathon_client.get_app.assert_called_with("/app/foo")
+            #self.assertDictEqual(modified_app, filtered_request.get_json())
+
+            self.assertEqual(
+                filtered_request.get_json(),
+                {
+                    "id": "/foo/bar",
+                    "constraints": [
+                        [
+                            "exclusive",
+                            "UNLIKE",
+                            ".*"
+                        ]
                     ]
-                ]
-            }
-        )
+                }
+            )
 
     def test_simple_app_with_constraints(self):
         data = {
@@ -41,19 +50,24 @@ class BaseConstraintFilterTest(TestCase):
                 ]
             ]
         }
-        request = RequestStub(data=data)
-        modified_request = self.filter.run(request)
 
-        self.assertEqual(
-            modified_request.get_json(),
-            {
-                "id": "/foo/bar",
-                "constraints": [
-                    [
-                        "exclusive",
-                        "LIKE",
-                        "web"
+        from marathon.models.app import MarathonApp
+        with mock.patch.object(self.filter, "ctx") as ctx_mock:
+            request = RequestStub(path="/v2/apps//app/foo", data=data, method="PUT")
+            ctx_mock.marathon_client.get_app.return_value = MarathonApp(**data)
+
+            modified_request = self.filter.run(request)
+
+            self.assertEqual(
+                modified_request.get_json(),
+                {
+                    "id": "/foo/bar",
+                    "constraints": [
+                        [
+                            "exclusive",
+                            "LIKE",
+                            "web"
+                        ]
                     ]
-                ]
-            }
-        )
+                }
+            )
