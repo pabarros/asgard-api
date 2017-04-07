e3b2172a55c65e824c4d5b3248322e6fd492849f
Dalton Barreto <daltonmatos@sieve.com.br>
2017-04-07 15:51:51 -0300

    Removendo código morto
    IN-2140

--- hollowman/conf.py
+++ hollowman/conf.py
@@ -5,8 +5,6 @@ import base64
 
 from marathon import MarathonClient
 
-ENABLED = "1"
-
 MARATHON_ENDPOINT = os.getenv("MARATHON_ENDPOINT", "http://127.0.0.1:8080")
 MARATHON_CREDENTIALS = os.getenv("MARATHON_CREDENTIALS", "guest:guest")
 
@@ -14,9 +12,6 @@ MARATHON_AUTH_HEADER = "Basic {}".format(base64.b64encode(MARATHON_CREDENTIALS))
 
 user, passw = MARATHON_CREDENTIALS.split(':')
 marathon_client = MarathonClient([MARATHON_ENDPOINT], username=user, password=passw)
-
-# Default enabled
-FILTER_DNS_ENABLED = os.getenv("HOLLOWMAN_FILTER_DNS_ENABLE", "1") == "1"
 
 def _build_cors_whitelist(env_value):
     if not env_value:
