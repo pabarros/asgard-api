ecd96e70d0ec3dc7a0af4de14ecd002b199a028f
Dalton Barreto <daltonmatos@sieve.com.br>
2017-08-15 10:22:34 -0300

    Migrando de Flask-OAuth para Flask-OAuthlib
    Flask-OAuth não é mais mantido.
    
    IN-2245

--- hollowman/auth/google.py
+++ hollowman/auth/google.py
@@ -1,6 +1,6 @@
 #encoding: utf-8
 
-from flask_oauth import OAuth
+from flask_oauthlib.client import OAuth
 
 from hollowman.conf import GOOGLE_OAUTH2_CLIENT_ID, GOOGLE_OAUTH2_CLIENT_SECRET
 oauth = OAuth()
@@ -8,11 +8,9 @@ google_oauth2 = oauth.remote_app('google',
                           base_url='https://www.google.com/accounts/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
                           request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
-                                                'response_type': 'code'},
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',},
                           access_token_url='https://accounts.google.com/o/oauth2/token',
                           access_token_method='POST',
-                          access_token_params={'grant_type': 'authorization_code'},
                           consumer_key=GOOGLE_OAUTH2_CLIENT_ID,
                           consumer_secret=GOOGLE_OAUTH2_CLIENT_SECRET
                 )
