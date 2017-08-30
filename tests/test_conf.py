a1fcd08fc6b156b1f315e0f0dedb48d9d02c024a
diogommartins <magalhaesmartins@icloud.com>
2017-08-30 10:51:14 -0300

    Refatorando imports em teste

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -1,23 +1,15 @@
 import unittest
-import mock
-import os
 
-import mock
-from unittest import TestCase, skip
 from hollowman import conf
-from marathon import MarathonApp
-import os
 
-#from hollowman.conf import _build_cors_whitelist
-import hollowman
 
 class ConfTest(unittest.TestCase):
 
     def test_build_cors_whitelist(self):
-        self.assertEqual([], hollowman.conf._build_cors_whitelist(""))
-        self.assertEqual([], hollowman.conf._build_cors_whitelist(None))
-        self.assertEqual(["name.com.br"], hollowman.conf._build_cors_whitelist("name.com.br"))
-        self.assertEqual(["name.com.br", "other.com.br"], hollowman.conf._build_cors_whitelist("name.com.br,other.com.br"))
-        self.assertEqual(["name.com.br", "other.com.br"], hollowman.conf._build_cors_whitelist("  name.com.br,  other.com.br  "))
-        self.assertEqual(["name.com.br"], hollowman.conf._build_cors_whitelist("name.com.br,"))
-        self.assertEqual(["name.com.br"], hollowman.conf._build_cors_whitelist(",name.com.br"))
+        self.assertEqual([], conf._build_cors_whitelist(""))
+        self.assertEqual([], conf._build_cors_whitelist(None))
+        self.assertEqual(["name.com.br"], conf._build_cors_whitelist("name.com.br"))
+        self.assertEqual(["name.com.br", "other.com.br"], conf._build_cors_whitelist("name.com.br,other.com.br"))
+        self.assertEqual(["name.com.br", "other.com.br"], conf._build_cors_whitelist("  name.com.br,  other.com.br  "))
+        self.assertEqual(["name.com.br"], conf._build_cors_whitelist("name.com.br,"))
+        self.assertEqual(["name.com.br"], conf._build_cors_whitelist(",name.com.br"))
