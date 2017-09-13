4a7cff018b58e50f0dbe223de501fe8503354564
Dalton Barreto <daltonmatos@sieve.com.br>
2017-09-13 08:37:31 -0300

    Removendo header Content-Encoding
    O Jetty do Marathon 1.3.13 retorna todos os responses em formato gzip.
    Como o hollowman faz o ungzip no meio do caminho (e ainda *não refaz o
    gzip), precisamos remover esse header. Não remover o header causa
    problemas para quem esivver recebendo o response, ja que um header
    indica que o conteúdo é gzip, quando na veradade não é.
    
    IN-2284 IN-2285

--- hollowman/routes.py
+++ hollowman/routes.py
@@ -32,6 +32,7 @@ def apiv2(path):
     r = upstream.replay_request(modded_request, conf.MARATHON_ENDPOINT)
     h = dict(r.headers)
     h.pop("Transfer-Encoding", None)
+    h.pop("Content-Encoding", None) # Marathon 1.3.x returns all responses gziped
     return Response(response=r.content, status=r.status_code, headers=h)
 
 @application.route("/healthcheck")
