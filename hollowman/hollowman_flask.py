42449b0a6d6162e904e0bdc9c0ba60232112446d
diogommartins <magalhaesmartins@icloud.com>
2017-09-19 13:41:14 -0300

    Adding operations property to HollowmanRequest

--- hollowman/hollowman_flask.py
+++ hollowman/hollowman_flask.py
@@ -1,6 +1,21 @@
 from flask import Flask,Request
 from json import loads
 
+from enum import Enum, auto
+
+
+class OperationType(Enum):
+    READ = auto()
+    WRITE = auto()
+
+    SCALE_UP = auto()
+    SCALE_DOWN = auto()
+    RESTART = auto()
+    DESTROY = auto()
+    SUSPEND = auto()
+    CREATE = auto()
+
+
 class HollowmanRequest(Request):
 
     def get_json(self, cache=False, **kwargs):
@@ -12,6 +27,18 @@ class HollowmanRequest(Request):
         #     cache=False,
         #     **kwargs
         # )
+
+    @property
+    def operations(self):
+        operations = []
+
+        if self.method == 'GET':
+            operations.append(OperationType.READ)
+        elif self.method in ('POST', 'PUT', 'PATCH', 'DELETE'):
+            operations.append(OperationType.WRITE)
+
+        return operations
+
 
 #  See: http://flask.pocoo.org/docs/0.12/patterns/subclassing/
 class HollowmanFlask(Flask):
