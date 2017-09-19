91bb0cfc285c545be2b2463965f106b3e60a7a3d
diogommartins <magalhaesmartins@icloud.com>
2017-09-19 13:55:25 -0300

    Removendo manipulação de headers

--- hollowman/request_handlers.py
+++ hollowman/request_handlers.py
@@ -13,13 +13,9 @@ def old(request: HollowmanRequest) -> Response:
         import traceback
     
         traceback.print_exc()
+
     r = upstream.replay_request(modded_request, conf.MARATHON_ENDPOINT)
-    h = dict(r.headers)
-    h.pop("Transfer-Encoding", None)
-    # Marathon 1.3.x returns all responses gziped
-    h.pop("Content-Encoding", None)
-    
-    return Response(response=r.content, status=r.status_code, headers=h)
+    return Response(response=r.content, status=r.status_code, headers=r.headers)
 
 
 def new(request: HollowmanRequest) -> Response:
