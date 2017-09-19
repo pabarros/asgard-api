ad8515ffa5cfa2dfcde4290cc8b61ef3975e94bd
diogommartins <magalhaesmartins@icloud.com>
2017-09-19 15:34:00 -0300

    Removendo import não utilizado

--- hollowman/decorators.py
+++ hollowman/decorators.py
@@ -1,5 +1,3 @@
-#encoding: utf-8
-
 from functools import wraps
 from collections import defaultdict
 
@@ -8,7 +6,6 @@ import json
 from flask import request, make_response
 from alchemytools.context import managed
 
-from hollowman.auth.jwt import jwt_auth
 from hollowman.conf import SECRET_KEY, HOLLOWMAN_ENFORCE_AUTH
 from hollowman.models import HollowmanSession, User
 from hollowman.log import logger
