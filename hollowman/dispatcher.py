6370bc961d96749301ce61ca70ee387fbe9a901b
diogommartins <magalhaesmartins@icloud.com>
2017-09-24 14:48:58 -0300

    Adding dummy dispatcher implementation

--- hollowman/dispatcher.py
+++ hollowman/dispatcher.py
@@ -3,7 +3,8 @@ from typing import Iterable
 from marathon import MarathonApp
 
 from hollowman.filters.dummy import DummyLogFilter
-from hollowman.hollowman_flask import HollowmanRequest, OperationType
+from hollowman.hollowman_flask import OperationType
+from hollowman.log import logger
 
 
 class InterruptPipelineException(Exception):
@@ -12,33 +13,21 @@ class InterruptPipelineException(Exception):
         self.status_code = status_code
 
 
-class RequestParser:
-    def __init__(self, request: HollowmanRequest):
-        self.request = request
-
-
 filters_pipeline = {
     OperationType.READ: (DummyLogFilter(),)
 }
 
 
-def dispatch(self,
-             operations: Iterable[OperationType],
+def dispatch(operations: Iterable[OperationType],
              user,
              request_app: MarathonApp,
-             app: MarathonApp):
+             app: MarathonApp) -> MarathonApp:
+    logger.debug(request_app)
     for operation in operations:
         for filter_ in filters_pipeline[operation]:
-            func = getattr(filter_, operation)
+            func = getattr(filter_, operation.value)
             try:
                 request_app = func(user, request_app, app)
-            except InterruptPipelineException as e:
-                # Usamos a exception para criar a response ou
-                # retornar os dados para que alguém crie
-                pass
             except KeyError:
-                # skip. Unintersting operation
-                pass
-            except Exception:
-                # erros de programação que vão retornar um 500
-                pass
+                pass  # skip. Unintersting operation
+    return request_app
