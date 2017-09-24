4da13477692065181f0a33778cfe015f4f268577
diogommartins <magalhaesmartins@icloud.com>
2017-09-24 14:47:21 -0300

    Implementing request split -> filter -> join flow

--- hollowman/request_handlers.py
+++ hollowman/request_handlers.py
@@ -1,5 +1,7 @@
 from flask import Response
 
+from hollowman.dispatcher import dispatch
+from hollowman.parsers import RequestParser
 from hollowman.hollowman_flask import HollowmanRequest
 from hollowman import upstream, conf
 from hollowman.filters.request import RequestFilter
@@ -23,5 +25,19 @@ def old(request: HollowmanRequest) -> Response:
 
 
 def new(request: HollowmanRequest) -> Response:
-    # does nothing for now
-    return old(request)
+    request_parser = RequestParser(request)
+
+    if request_parser.is_group_request():
+        return old(request)
+
+    filtered_apps = []
+    for request_app, app in request_parser.split():
+        filtered_apps.append(
+            dispatch(operations=request.operations,
+                     user=None,
+                     request_app=request_app,
+                     app=app)
+        )
+
+    joined_request = request_parser.join(filtered_apps)
+    return old(joined_request)
