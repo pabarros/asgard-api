c3bed7c89117d00dfcbb652988b5e564008aa7fc
diogommartins <magalhaesmartins@icloud.com>
2017-09-24 14:55:36 -0300

    Adding write hook to dummy filter

--- hollowman/filters/dummy.py
+++ hollowman/filters/dummy.py
@@ -8,3 +8,6 @@ class DummyLogFilter:
         logger.info({'info': 'Im a dummy filter', 'app': request_app})
         return request_app
 
+    def write(self, user, request_app: MarathonApp, app: MarathonApp) -> MarathonApp:
+        logger.info({'info': 'Im a dummy filter', 'app': request_app})
+        return request_app
