04491a8316974f39d09c00fb53bd52d65936b24b
diogommartins <magalhaesmartins@icloud.com>
2017-09-25 16:41:56 -0300

    Adding request handler tests

---
+++ tests/test_request_handlers.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from marathon import MarathonApp
+
+from hollowman.app import application
+from hollowman.request_handlers import new
+
+
+class RequestHandlersTests(TestCase):
+    def setUp(self):
+        parser_patch = patch('hollowman.request_handlers.RequestParser')
+        self.parser = parser_patch.start()
+        self.parser.return_value.is_group_request.return_value = False
+
+        self.request_apps = [
+            (MarathonApp(id='/xablau'), MarathonApp(id='/xena')),
+            (MarathonApp(id='/foo'), MarathonApp(id='/bar')),
+        ]
+        self.parser.return_value.split.return_value = self.request_apps
+
+        dispatch_patch = patch('hollowman.request_handlers.dispatch',
+                               side_effect=lambda *args, **kwargs: kwargs['request_app'])
+        self.dispatch = dispatch_patch.start()
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_it_calls_the_old_handler_for_group_requests(self):
+        with application.test_request_context('/v2/groups/', method='GET') as ctx:
+            with patch('hollowman.request_handlers.old') as old:
+                new(ctx.request)
+                old.assert_called_once_with(ctx.request)
+
+    def test_it_dispatches_apps_from_request(self):
+        with application.test_request_context('/v2/apps/foo', method='GET') as ctx:
+            with patch('hollowman.request_handlers.old') as old:
+                response = new(ctx.request)
+
+                self.assertEqual(response, old.return_value)
+                self.parser.return_value.split.assert_called_once()
+                self.parser.return_value.join.assert_called_once_with(self.request_apps)
