c86cf793d10355625057ade0278bfa0697804f1d
diogommartins <magalhaesmartins@icloud.com>
2017-10-02 13:57:55 -0300

    Adding filter implementation

--- hollowman/filters/trim.py
+++ hollowman/filters/trim.py
@@ -1,6 +1,10 @@
 #encoding: utf-8
 
 import json
+from typing import Dict
+
+from marathon import MarathonApp
+
 from hollowman.filters import BaseFilter
 
 class TrimRequestFilter(BaseFilter):
@@ -25,3 +29,20 @@ class TrimRequestFilter(BaseFilter):
 
             request.data = json.dumps(data)
         return request
+
+
+class TrimEnvvarsFilter:
+    app_attrs_to_trim = ('labels', 'env')
+
+    def _trim_dict_items(self, d: Dict[str, str]) -> Dict[str, str]:
+        new = {}
+        for k, v in d.items():
+            new[k.strip()] = v.strip()
+        return new
+
+    def write(self, user, request_app: MarathonApp, app: MarathonApp) -> MarathonApp:
+        for name in self.app_attrs_to_trim:
+            attr = getattr(request_app, name)
+            if attr:
+                setattr(request_app, name, self._trim_dict_items(attr))
+        return request_app
