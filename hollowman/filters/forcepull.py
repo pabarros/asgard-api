407fcc076ca79df8037c6493291fa0c65b9c0f1c
Dalton Barreto <daltonmatos@sieve.com.br>
2017-10-03 16:04:47 -0300

    Removendo código velho do filtro forcepull
    IN-2320

--- hollowman/filters/forcepull.py
+++ hollowman/filters/forcepull.py
@@ -9,22 +9,6 @@ class ForcePullFilter():
 
     name = 'pull'
 
-    def run(self, ctx):
-        request = ctx.request
-        if request.is_json and request.data:
-            data = request.get_json()
-
-            if self.is_single_app(data):
-                original_app_dict = json.loads(self.get_original_app(ctx).to_json())
-                original_app_dict.update(data)
-
-                if self.is_docker_app(original_app_dict):
-                    original_app_dict['container']['docker']["forcePullImage"] = True
-
-                request.data = json.dumps(original_app_dict)
-
-        return request
-
     def write(sef, user, request_app, app):
         try:
             request_app.container.docker.force_pull_image = True
--- tests/filters/test_forcepull.py
+++ tests/filters/test_forcepull.py
@@ -2,8 +2,6 @@
 
 from hollowman.filters.forcepull import ForcePullFilter
 from unittest import TestCase
-from tests import RequestStub
-from hollowman.filters import Context
 
 from marathon.models.app import MarathonApp
 import mock
@@ -13,103 +11,7 @@ from tests.utils import with_json_fixture
 class ForcePullTest(TestCase):
 
     def setUp(self):
-        self.ctx = Context(marathon_client=None, request=None)
         self.filter = ForcePullFilter()
-
-    def test_simple_app(self):
-        data = {
-            "id": "/foo/bar",
-            "container":{
-                "docker":{
-                    "image": "alpine:3.4"
-                }
-            }
-        }
-        with mock.patch.object(self, "ctx") as ctx_mock:
-            request = RequestStub(path="/v2/apps//app/foo", data=data, method="PUT")
-            ctx_mock.marathon_client.get_app.return_value = MarathonApp(**data)
-
-            self.ctx.request = request
-            modified_request = self.filter.run(self.ctx)
-            self.assertEqual(
-                True,
-                modified_request.get_json()['container']['docker']['forcePullImage']
-            )
-
-    def test_app_with_fields(self):
-        data = {
-            "id": "/foo/bar",
-            "cmd": "sleep 5000",
-            "cpus": 1,
-            "mem": 128,
-            "disk": 0,
-            "instances": 1,
-            "container": {
-                "docker": {
-                    "image": "alpine:3.4"
-                },
-                "volumes": [
-                    {
-                        "containerPath": "data",
-                        "persistent": {
-                            "size": 128
-                        },
-                        "mode": "RW"
-                    }
-                ],
-                "type": "MESOS"
-            }
-        }
-
-        with mock.patch.object(self, "ctx") as ctx_mock:
-            request = RequestStub(path="/v2/apps//app/foo", data=data, method="PUT")
-            ctx_mock.marathon_client.get_app.return_value = MarathonApp(**data)
-
-            self.ctx.request = request
-            modified_request = self.filter.run(self.ctx)
-            self.assertTrue(
-                modified_request.get_json()['container']['docker']['forcePullImage']
-            )
-            self.assertEqual("data", modified_request.get_json()['container']['volumes'][0]['containerPath'])
-            self.assertTrue("type" in modified_request.get_json()['container'])
-
-    def test_app_creation(self):
-        data = {
-            "id": "/foo/bar",
-            "cmd": "sleep 5000",
-            "cpus": 1,
-            "mem": 128,
-            "disk": 0,
-            "instances": 1,
-            "container": {
-                "docker": {
-                    "image": "alpine:3.4"
-                },
-                "volumes": [
-                    {
-                        "containerPath": "data",
-                        "persistent": {
-                            "size": 128
-                        },
-                        "mode": "RW"
-                    }
-                ],
-                "type": "MESOS"
-            }
-        }
-
-        with mock.patch.object(self, "ctx") as ctx_mock:
-            request = RequestStub(path="/v2/apps/", data=data, method="POST")
-            # if the app does not exist, get_app() returns an empty marathon.models.app.MarathonApp()
-            ctx_mock.marathon_client.get_app.return_value = MarathonApp()
-
-            self.ctx.request = request
-            modified_request = self.filter.run(self.ctx)
-            self.assertTrue(
-                modified_request.get_json()['container']['docker']['forcePullImage']
-            )
-            self.assertEqual("data", modified_request.get_json()['container']['volumes'][0]['containerPath'])
-            self.assertTrue("type" in modified_request.get_json()['container'])
 
     @with_json_fixture("single_full_app.json")
     def test_app_exists_forcepull_not_checked(self, single_full_app_fixture):
