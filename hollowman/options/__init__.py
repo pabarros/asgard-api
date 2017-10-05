59440266b3d0bad7f96dd512a82748eb7957c6bb
Dalton Barreto <daltonmatos@sieve.com.br>
2017-10-05 14:36:50 -0300

    Tornando o mecanismo de get_options mais genérico
    IN-2300
    
    Antes ela era específico para os filtros. Agora ele é apenas separado
    por namespaces genéricos.

--- hollowman/options/__init__.py
+++ hollowman/options/__init__.py
@@ -2,10 +2,10 @@
 
 import os
 
-ENV_NAME_PATTERN = "HOLLOWMAN_FILTER_{filter_name}_PARAM_{option}"
+ENV_NAME_PATTERN = "HOLLOWMAN_{namespace}_{option_name}"
 
 
-def get_option(filter_name, option_name):
+def get_option(namespace, option_name):
     """
     Gets an options based on filter and option name.
     All option are read from Environ variables. The formation name is:
@@ -14,12 +14,12 @@ def get_option(filter_name, option_name):
         HOLLOWMAN_FILTER_<FILTERNAME>_PARAM_<OPTIONNAME>_<INDEX>
 
 
-    :filter_name: Name of the filter who owns this option
+    :name: Name of the filter who owns this option
     :option_name: Name of the option
     :returns: The option value, **always** as a list of values
 
     """
-    envvalue = _get_env_value(filter_name, option_name)
+    envvalue = _get_env_value(namespace, option_name)
 
     idx = 0
     final_value = []
@@ -27,14 +27,14 @@ def get_option(filter_name, option_name):
     if envvalue:
         final_value.append(envvalue)
 
-    while _get_env_value(filter_name, option_name, idx):
-        final_value.append(_get_env_value(filter_name, option_name, idx))
+    while _get_env_value(namespace, option_name, idx):
+        final_value.append(_get_env_value(namespace, option_name, idx))
         idx += 1
 
     return final_value
 
-def _get_env_value(filter_name, option_name, idx=None):
-    base_envname = ENV_NAME_PATTERN.format(filter_name=filter_name.upper(), option=option_name.upper())
+def _get_env_value(namespace, option_name, idx=None):
+    base_envname = ENV_NAME_PATTERN.format(namespace=namespace.upper(), option_name=option_name.upper())
     if idx is None:
         return os.getenv(base_envname)
     return os.getenv("{}_{}".format(base_envname, idx))
