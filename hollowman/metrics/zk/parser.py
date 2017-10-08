03aff9c31091f1edd1e0c6f86407fcea2d98d5b3
diogommartins <magalhaesmartins@icloud.com>
2017-10-08 17:48:57 -0300

    refactoring and removing defaultdict/dummy_parser logic

--- hollowman/metrics/zk/parser.py
+++ hollowman/metrics/zk/parser.py
@@ -1,11 +1,3 @@
-
-
-from collections import defaultdict
-
-
-def _dummy_parser(data):
-    return {}
-
 def _parse_latency_line(data):
     _min, _avg, _max = data.split("/")
     return {
@@ -14,6 +6,7 @@ def _parse_latency_line(data):
         "latency_max": int(_max),
     }
 
+
 def _parse_mode_line(data):
     value = data.strip()
     return {
@@ -21,18 +14,19 @@ def _parse_mode_line(data):
         "is_leader": 1 if (value == "leader") else 0
     }
 
+
 def _parse_connections(data):
     return {"connections": int(data)}
+
 
 def _parse_oustanting(data):
     return {"outstanding": int(data)}
 
+
 def _parse_node_count(data):
     return {"node_count": int(data)}
 
-STAT_LINE_PARSER_CALLBACK = defaultdict(lambda: _dummy_parser)
-
-_call_backs = {
+STAT_LINE_PARSER_CALLBACK = {
     'latency min/avg/max': _parse_latency_line,
     'connections': _parse_connections,
     'outstanding': _parse_oustanting,
@@ -40,15 +34,18 @@ _call_backs = {
     'node count': _parse_node_count,
 }
 
-STAT_LINE_PARSER_CALLBACK.update(_call_backs)
 
 def parse_stat_output(raw_output):
     result_dict = {}
     lines = [l.lower() for l in raw_output.split("\n") if ":" in l]
     for line in lines:
         key, value = line.split(":", 1)
-        parsed = STAT_LINE_PARSER_CALLBACK[key.strip()](value)
-        result_dict.update(parsed)
+        try:
+            parser = STAT_LINE_PARSER_CALLBACK[key.strip()]
+        except KeyError:
+            continue
+        else:
+            parsed = parser(value)
+            result_dict.update(parsed)
 
     return result_dict
-
