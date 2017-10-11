6bb5fed04205446d97461b0d94636172c3d9ea6c
Dalton Barreto <daltonmatos@gmail.com>
2017-10-11 15:17:39 -0300

    Teste para o caso de um request não autenticado
    IN-2273

--- hollowman/filters/owner.py
+++ hollowman/filters/owner.py
@@ -6,6 +6,9 @@ class AddOwnerConstraintFilter():
     name = "owner"
 
     def write(self, user, request_app, original_app):
+        if not user:
+            return request_app
+
         owner_constraint = MarathonConstraint(field="owner", operator="LIKE", value=user.current_account.owner)
         current_owner_constraint = request_app.get_constraint_by_name("owner")
         if current_owner_constraint:
