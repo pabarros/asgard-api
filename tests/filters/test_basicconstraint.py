33013133f2479da6ef529037559b63acb62ba834
diogommartins <magalhaesmartins@icloud.com>
2017-11-07 14:38:05 -0200

    Adicionando mais casos de teste

--- tests/filters/test_basicconstraint.py
+++ tests/filters/test_basicconstraint.py
@@ -18,9 +18,24 @@ class TestBasicConstraintFilter(unittest.TestCase):
     def test_it_adds_default_constraints_if_none_present(self):
         self.request_app.constraints = []
         result_app = self.filter.write(self.user, self.request_app, self.original_app)
-        self.assertEqual(self.request_app.constraints, list(self.filter.constraints))
+        self.assertEqual(result_app.constraints, list(self.filter.constraints))
 
     def test_it_doesnt_change_current_constraints_if_constraints_isnt_empty(self):
         original_constraints = deepcopy(self.request_app.constraints)
         result_app = self.filter.write(self.user, self.request_app, self.original_app)
-        self.assertEqual(original_constraints, self.request_app.constraints)
+        self.assertEqual(original_constraints, result_app.constraints)
+
+    def test_request_app_has_all_constraints_original_app_constraints_is_empty(self):
+        self.request_app.constraints = list(self.filter.constraints)
+        self.original_app.constraints = []
+        result_app = self.filter.write(self.user, self.request_app, self.original_app)
+
+        self.assertEqual(result_app.constraints, list(self.filter.constraints))
+
+    def test_request_app_has_single_constraint_original_app_constraints_is_empty(self):
+        self.request_app.constraints = [self.filter.constraints[0]]
+        self.original_app.constraints = []
+        result_app = self.filter.write(self.user, self.request_app,
+                                       self.original_app)
+
+        self.assertEqual(result_app.constraints, [self.filter.constraints[0]])
