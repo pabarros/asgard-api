41800a4aef3526416235dec70222c17e71dfd4f3
Dalton Barreto <daltonmatos@gmail.com>
2017-11-07 08:06:30 -0200

    Implementação do script que ajuda no restore do backup
    IN-2347
    
    O json que o marathon retorna, vem com alguns campos que ele mesmo
    (Marathon) não aceita quando fazemod o POST de volta.
    
    Esse script remove esses campos. Para restaurar o backup todo de uma vez
    devemos filtrar o backup com esse script antes de enviar para a API do
    Marathon.
    
    Exemplo:
    
    PYTHONPATH=. python prepare-backup.py ../dumps/marathon-infra-raw-2017-11-06.json | curl -X POST -d@- -H "Content-type: application/json" http://localhost:8080/v2/groups/

---
+++ prepare-backup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Script que percorre todas as apps e groups de um json (de Grupo) removendo:
+# app.version
+# app.fetch
+# group.version
+
+import sys
+import json
+
+from hollowman.marathon.group import SieveAppGroup
+from marathon.models.group import MarathonGroup
+from marathon.util import MarathonJsonEncoder
+
+
+data = open(sys.argv[1]).read()
+_g = SieveAppGroup(MarathonGroup().from_json(json.loads(data)))
+
+for group in _g.iterate_groups():
+    del group.version
+    for app in group.apps:
+        del app.version
+        del app.fetch
+
+data_output = json.dumps(_g._marathon_group, cls=MarathonJsonEncoder)
+print (data_output)
