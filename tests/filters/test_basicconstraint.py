4fb262b8e5f83a4694b296495130ef34b93ef8d2
diogommartins <magalhaesmartins@icloud.com>
2017-11-08 17:46:53 -0200

    Adicionando casos de teste para basic constraint

--- tests/filters/test_basicconstraint.py
+++ tests/filters/test_basicconstraint.py
@@ -2,6 +2,8 @@ import unittest
 from copy import deepcopy
 from unittest.mock import Mock
 
+from marathon import MarathonConstraint
+
 from hollowman.filters.basicconstraint import BasicConstraintFilter
 from hollowman.marathonapp import SieveMarathonApp
 from tests.utils import get_fixture
@@ -23,7 +25,8 @@ class TestBasicConstraintFilter(unittest.TestCase):
     def test_it_doesnt_change_current_constraints_if_constraints_isnt_empty(self):
         original_constraints = deepcopy(self.request_app.constraints)
         result_app = self.filter.write(self.user, self.request_app, self.original_app)
-        self.assertEqual(original_constraints, result_app.constraints)
+        self.assertEqual(result_app.constraints,
+                         original_constraints + list(self.filter.constraints))
 
     def test_request_app_has_all_constraints_original_app_constraints_is_empty(self):
         self.request_app.constraints = list(self.filter.constraints)
@@ -38,4 +41,32 @@ class TestBasicConstraintFilter(unittest.TestCase):
         result_app = self.filter.write(self.user, self.request_app,
                                        self.original_app)
 
-        self.assertEqual(result_app.constraints, [self.filter.constraints[0]])
+        self.assertEqual(result_app.constraints, list(self.filter.constraints))
+
+    def test_it_adds_only_mesos_constraint_if_request_app_has_workload_constraint(self):
+        self.request_app.constraints = [
+            MarathonConstraint.from_json('workload:LIKE:xablau'.split(':'))
+        ]
+
+        result_app = self.filter.write(self.user, self.request_app, self.original_app)
+        self.assertEqual(
+            result_app.constraints,
+            [
+                MarathonConstraint.from_json('workload:LIKE:xablau'.split(':')),
+                self.filter.mesos_constraint
+            ]
+        )
+
+    def test_it_adds_only_workload_constraint_if_request_app_has_mesos_constraint(self):
+        self.request_app.constraints = [
+            MarathonConstraint.from_json('mesos:LIKE:xena'.split(':')),
+        ]
+        result_app = self.filter.write(self.user, self.request_app, self.original_app)
+
+        self.assertEqual(
+            result_app.constraints,
+            [
+                MarathonConstraint.from_json('mesos:LIKE:xena'.split(':')),
+                self.filter.workload_constraint
+            ]
+        )
