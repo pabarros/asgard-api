0c0337cbd00ec8fe70215b896349db03f092c69c
diogommartins <magalhaesmartins@icloud.com>
2017-11-09 10:36:00 -0200

    Alterando BasicConstraint para utilizar os métodos de SieveMarathonApp

--- hollowman/filters/basicconstraint.py
+++ hollowman/filters/basicconstraint.py
@@ -1,5 +1,7 @@
 from marathon import MarathonConstraint
 
+from hollowman.marathonapp import SieveMarathonApp
+
 
 class BasicConstraintFilter:
     mesos_constraint = MarathonConstraint.from_json('mesos:LIKE:slave'.split(':'))
@@ -7,21 +9,14 @@ class BasicConstraintFilter:
 
     constraints = (mesos_constraint, workload_constraint)
 
-    def write(self, user, request_app, app):
+    def write(self, user, request_app: SieveMarathonApp, app):
         if not request_app.constraints:
             request_app.constraints = list(self.constraints)
             return request_app
 
-        has_mesos_constraint, has_workload_constraint = False, False
-        for constraint in request_app.constraints:
-            if constraint.field == self.mesos_constraint.field:
-                has_mesos_constraint = True
-            elif constraint.field == self.workload_constraint.field:
-                has_workload_constraint = True
-
-        if not has_mesos_constraint:
+        if not request_app.has_constraint(self.mesos_constraint.field):
             request_app.constraints.append(self.mesos_constraint)
-        if not has_workload_constraint:
+        if not request_app.has_constraint(self.workload_constraint.field):
             request_app.constraints.append(self.workload_constraint)
 
         return request_app
