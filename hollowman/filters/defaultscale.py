0ac1a1e099f0a7b6a98301f298f0d7f2e17f05bc
Dalton Barreto <daltonmatos@gmail.com>
2017-11-09 15:12:34 -0200

    Immplementação do filtro de default scale
    IN-2322

--- hollowman/filters/defaultscale.py
+++ hollowman/filters/defaultscale.py
@@ -5,4 +5,6 @@ class DefaultScaleFilter:
     name = "defaultscale"
 
     def write(self, user, request_app, original_app):
+        if original_app.instances and request_app.instances == 0:
+            request_app.labels["hollowman.default_scale"] = original_app.instances
         return request_app
--- tests/filters/test_defaultscale.py
+++ tests/filters/test_defaultscale.py
@@ -11,7 +11,7 @@ from http import HTTPStatus
 from hollowman.app import application
 from hollowman.models import Account, User
 from hollowman.marathonapp import SieveMarathonApp
-from hollowman.filters.namespace import NameSpaceFilter
+from hollowman.filters.defaultscale import DefaultScaleFilter
 from hollowman.http_wrappers.response import Response
 from tests.utils import with_json_fixture
 
@@ -19,7 +19,7 @@ class TestNamespaceFilter(unittest.TestCase):
 
     @with_json_fixture("single_full_app.json")
     def setUp(self, single_full_app_fixture):
-        self.filter = NameSpaceFilter()
+        self.filter = DefaultScaleFilter()
         self.request_app = SieveMarathonApp.from_json(single_full_app_fixture)
         self.original_app = SieveMarathonApp.from_json(single_full_app_fixture)
         self.account = Account(name="Dev Account", namespace="dev", owner="company")
@@ -27,10 +27,29 @@ class TestNamespaceFilter(unittest.TestCase):
         self.user.current_account = self.account
 
     def test_create_app_with_zero_instances(self):
-        self.fail()
+        self.request_app.instances = 0
+        filtered_app = self.filter.write(self.user, self.request_app, SieveMarathonApp())
+        self.assertTrue("hollowman.default_scale" not in filtered_app.labels.keys(), "Não deveria ter adicionado label default_scale")
 
     def test_update_suspended_app_set_instances_to_zero(self):
-        self.fail()
+        """
+        Nesse caso não devemos mexer no default scale.
+        """
+        self.request_app.instances = 0
+        self.original_app.instances = 0
+        self.request_app.labels["hollowman.default_scale"] = 5
+        filtered_app = self.filter.write(self.user, self.request_app, self.original_app)
+        self.assertEqual(5, filtered_app.labels["hollowman.default_scale"])
 
     def test_update_running_app_set_instances_to_zero(self):
-        self.fail()
+        self.original_app.instances = 10
+        self.request_app.instances = 0
+        filtered_app = self.filter.write(self.user, self.request_app, self.original_app)
+        self.assertEqual(10, filtered_app.labels["hollowman.default_scale"])
+
+    def test_scale_down_should_not_change_default_scale(self):
+        self.original_app.instances = 10
+        self.request_app.instances = 7
+        filtered_app = self.filter.write(self.user, self.request_app, self.original_app)
+        self.assertTrue("hollowman.default_scale" not in filtered_app.labels.keys(), "Não deveria ter adicionado label default_scale")
+
