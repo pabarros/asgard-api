4eef5263d9e8a42212cb73b3d64315e1d4658368
Dalton Barreto <daltonmatos@gmail.com>
2017-11-14 17:40:46 -0200

    Teste de um acesso com falha de auth, retornando 401
    IN-2349

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,7 @@ from hollowman import conf
 from hollowman import decorators
 import hollowman.upstream
 from hollowman.upstream import replay_request
-from hollowman.models import HollowmanSession, User
+from hollowman.models import HollowmanSession, User, Account
 
 from tests import rebuild_schema
 from tests.utils import with_json_fixture
@@ -29,7 +29,11 @@ class TestApp(TestCase):
     def setUp(self, fixture):
         rebuild_schema()
         self.session = HollowmanSession()
-        self.session.add(User(tx_email="user@host.com.br", tx_name="John Doe", tx_authkey="69ed620926be4067a36402c3f7e9ddf0"))
+        self.user = User(tx_email="user@host.com.br", tx_name="John Doe", tx_authkey="69ed620926be4067a36402c3f7e9ddf0")
+        self.session.add(self.user)
+        self.account_dev = Account(id=4, name="Dev Team", namespace="dev", owner="company")
+        self.session.add(self.account_dev)
+        self.user.accounts = [self.account_dev]
         self.session.commit()
         responses.add(method='GET',
                          url=conf.MARATHON_ENDPOINT + '/v2/apps',
@@ -41,6 +45,14 @@ class TestApp(TestCase):
     def tearDown(self):
         self.session.close()
         responses.stop()
+
+    def test_auth_error_returns_HTTP_401(self):
+        with application.test_client() as client:
+            response = client.get("/v2/apps")
+            self.assertEqual(401, response.status_code)
+
+    def test_unexpected_error_returns_HTTP_500(self):
+        self.fail()
 
     def test_remove_transfer_encoding_header(self):
         with application.test_request_context("/v2/apps", method="GET") as ctx:
