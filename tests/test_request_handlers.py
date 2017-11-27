b00971375f363b858887f9ea381b630bc0dd4410
Dalton Barreto <daltonmatos@gmail.com>
2017-11-27 12:07:37 -0200

    Ajustes nos teste do request_handler
    Ajustando os testes para se adequar ao refactor que foi feito no
    request_dispatch().
    
    IN-2344 IN-2364

--- tests/test_request_handlers.py
+++ tests/test_request_handlers.py
@@ -20,18 +20,11 @@ class RequestHandlersTests(TestCase):
 
     @with_json_fixture("single_full_app.json")
     def setUp(self, single_full_app_fixture):
-        self.parser = MagicMock()
-        self.parser.return_value.is_group_request.return_value = False
 
         self.request_apps = [
             (MarathonApp(id='/xablau'), MarathonApp(id='/xena')),
             (MarathonApp(id='/foo'), MarathonApp(id='/bar')),
         ]
-        self.parser.return_value.split.return_value = self.request_apps
-
-        dispatch_patch = patch('hollowman.request_handlers.dispatch',
-                               side_effect=lambda *args, **kwargs: kwargs['request_app'])
-        self.dispatch = dispatch_patch.start()
 
         rebuild_schema()
         self.session = HollowmanSession()
@@ -41,31 +34,6 @@ class RequestHandlersTests(TestCase):
         self.session.add(self.user)
         self.session.add(self.account_dev)
         self.session.commit()
-        responses.add(method='POST',
-                         url=conf.MARATHON_ENDPOINT + '/v2/apps/foo',
-                         body=json.dumps({'apps': [single_full_app_fixture]}),
-                         status=200)
-        responses.add(method='GET',
-                         url=conf.MARATHON_ENDPOINT + '/v2/apps/foo',
-                         body=json.dumps(single_full_app_fixture),
-                         status=200)
-        responses.start()
-
-    def tearDown(self):
-        responses.stop()
-        patch.stopall()
-
-    def test_it_dispatches_apps_from_request(self):
-        with application.test_request_context('/v2/apps/foo', method='GET') as ctx:
-            with patch('hollowman.request_handlers.upstream_request'), \
-                 patch('hollowman.request_handlers.dispatch_response_pipeline'):
-                request_parser = Request(ctx.request)
-                request_parser.split = MagicMock(return_value=self.request_apps)
-                request_parser.join = MagicMock()
-                response = new(request_parser)
-
-                request_parser.split.assert_called_once()
-                request_parser.join.assert_called_once_with(self.request_apps)
 
     def test_it_call_dispatch_using_user_from_request(self):
         """
@@ -73,6 +41,7 @@ class RequestHandlersTests(TestCase):
         """
         with application.test_request_context('/v2/apps/foo', method='GET') as ctx:
             with patch('hollowman.request_handlers.upstream_request'), \
+                 patch('hollowman.request_handlers.dispatch') as dispatch_mock, \
                  patch('hollowman.request_handlers.dispatch_response_pipeline'):
                 user = MagicMock()
                 ctx.request.user = user
@@ -80,23 +49,7 @@ class RequestHandlersTests(TestCase):
                 request_parser.split = MagicMock(return_value=[self.request_apps[0]])
                 request_parser.join = MagicMock()
                 response = new(request_parser)
-                self.dispatch.assert_called_once_with(operations=ANY,
-                                                user=user,
-                                                request_app=ANY,
-                                                app=ANY)
-
-    def test_it_calls_dispatch_with_user_none_if_not_authenticated(self):
-        with application.test_request_context('/v2/apps/foo', method='GET') as ctx:
-            with patch('hollowman.request_handlers.upstream_request'), \
-                 patch('hollowman.request_handlers.dispatch_response_pipeline'):
-                request_parser = Request(ctx.request)
-                request_parser.split = MagicMock(return_value=[self.request_apps[0]])
-                request_parser.join = MagicMock()
-                response = new(request_parser)
-                self.dispatch.assert_called_once_with(operations=ANY,
-                                                user=None,
-                                                request_app=ANY,
-                                                app=ANY)
+                dispatch_mock.assert_called_once_with(user=user, request=ANY)
 
     @with_json_fixture("../fixtures/single_full_app.json")
     def test_versions_endpoint_returns_app_on_root_json(self, single_full_app_fixture):
@@ -161,19 +114,6 @@ class DispatchResponse404Test(TestCase):
 
     @with_json_fixture("single_full_app.json")
     def setUp(self, single_full_app_fixture):
-        self.parser = MagicMock()
-        self.parser.return_value.is_group_request.return_value = False
-
-        self.request_apps = [
-            (MarathonApp(id='/xablau'), MarathonApp(id='/xena')),
-            (MarathonApp(id='/foo'), MarathonApp(id='/bar')),
-        ]
-        self.parser.return_value.split.return_value = self.request_apps
-
-        dispatch_patch = patch('hollowman.request_handlers.dispatch',
-                               side_effect=lambda *args, **kwargs: kwargs['request_app'])
-        self.dispatch = dispatch_patch.start()
-
         rebuild_schema()
         self.session = HollowmanSession()
         self.user = User(tx_email="user@host.com.br", tx_name="John Doe", tx_authkey="69ed620926be4067a36402c3f7e9ddf0")
