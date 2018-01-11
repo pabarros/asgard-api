95138ea88527259c22d7518c42027f9669a7d44d
diogommartins <magalhaesmartins@icloud.com>
2018-01-11 16:02:32 -0200

    Adicionando inicialização do agent do newrelic

--- hollowman/main.py
+++ hollowman/main.py
@@ -1,6 +1,10 @@
-#encoding: utf-8
-
+import newrelic.agent
 from hollowman.app import application
+from hollowman.conf import NEW_RELIC_LICENSE_KEY, NEW_RELIC_APP_NAME
+
 
 if __name__ == "__main__":
+    if NEW_RELIC_LICENSE_KEY and NEW_RELIC_APP_NAME:
+        newrelic.agent.initialize()
+
     application.run(host="0.0.0.0", port=5000, debug=True)
