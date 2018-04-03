75f854f4bb2599626e875d02d1cf8b55753636cd
Dalton Barreto <daltonmatos@gmail.com>
2018-04-03 13:22:49 -0300

    Flask com multi-thread.
    Dessa forma não temos apenas um worker atendendo as requisições.
    Útil para quando estamos debugando e precisamos servir os estáticos
    também.

--- hollowman/main.py
+++ hollowman/main.py
@@ -2,4 +2,4 @@ from hollowman.app import application
 
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=5000, debug=True)
+    application.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
