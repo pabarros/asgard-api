e1c067a5bf8105a41e793e8b0d5cb710e915ac4c
Danilo Pereira <developer@danilo.info>
2018-06-08 10:55:11 -0300

    Atualiza lista de usuários para me incluir

--- scripts/sql/user.sql
+++ scripts/sql/user.sql
@@ -6,6 +6,7 @@ insert into account (name, namespace, owner) values ('Asgard/INFRA', 'asgard-inf
 insert into "user" (tx_name, tx_email, tx_authkey, bl_system) values ('Rafael Amorim', 'rafael@admatic.com.br', 'cccccchcdhjrtrgbtndbedljrectjhv', false);
 INSERT INTO "user" (tx_name, tx_email, tx_authkey, bl_system) VALUES ('Dalton Barreto', 'daltonmatos@gmail.com', '15ce068d5d2942b18a7f8ae76933f53', false);
 
+INSERT INTO "user" (tx_name, tx_email, tx_authkey, bl_system) VALUES ('Danilo Pereira', 'danilo.nascimento@b2wdigital.com', 'd8c336eb8cca4d39a8425a151ac84f81', false);
 
 INSERT INTO user_has_account (account_id, user_id) VALUES (
   (SELECT id from account where name = 'Asgard/DEV'),
@@ -25,4 +26,14 @@ INSERT INTO user_has_account (account_id, user_id) VALUES (
 INSERT INTO user_has_account (account_id, user_id) VALUES (
   (SELECT id from account where name = 'Asgard/INFRA'),
   (SELECT id from "user" where tx_email='rafael@admatic.com.br')
+);
+
+INSERT INTO user_has_account (account_id, user_id) VALUES (
+  (SELECT id from account where name = 'Asgard/DEV'),
+  (SELECT id from "user" where tx_email='danilo.nascimento@b2wdigital.com')
+);
+
+INSERT INTO user_has_account (account_id, user_id) VALUES (
+  (SELECT id from account where name = 'Asgard/INFRA'),
+  (SELECT id from "user" where tx_email='danilo.nascimento@b2wdigital.com')
 );
