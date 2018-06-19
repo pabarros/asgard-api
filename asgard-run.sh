f51ba961d0d69e7322c5268429eb7f06f2786987
Dalton Barreto <daltonmatos@gmail.com>
2018-06-19 11:19:06 -0300

    Extraindo start do mesos slave
    IN-2561

--- asgard-run.sh
+++ asgard-run.sh
@@ -19,48 +19,7 @@ source "dev/zk.sh"
 source "dev/marathon.sh"
 source "dev/mesosmaster.sh"
 
-## MESOS SLAVE
-MESOS_ATTRIBUTES=";mesos:slave;workload:general"
-MESOS_RESOURCES="cpus(*):4;mem(*):4096;ports(*):[31000-31999];cpus(asgard):1;mem(asgard):1024;ports(asgard):[30000-30999]"
-MESOS_MASTER=zk://${ZK_CLUSTER_IPS}/mesos
-
-for SLAVE_IP in `echo ${MESOS_SLAVE_IPS_ACCOUNT_ASGARD_INFRA}`;
-do
-    echo -n "Mesos Slave (namespace=asgard-infra (${SLAVE_IP}) "
-    docker run -d --rm -it --ip ${SLAVE_IP} \
-      --name asgard_mesosslave_${RANDOM} \
-      --net ${NETWORK_NAME} \
-      --env-file <(
-    echo MESOS_IP=${SLAVE_IP}
-    echo LIBPROCESS_ADVERTISE_IP=${SLAVE_IP}
-    echo MESOS_ATTRIBUTES="${MESOS_ATTRIBUTES};owner:asgard-infra"
-    echo MESOS_MASTER=${MESOS_MASTER}
-    echo MESOS_RESOURCES=${MESOS_RESOURCES}
-    ) \
-      -v /sys/fs/cgroup:/sys/fs/cgroup \
-      -v /var/run/docker.sock:/var/run/docker.sock \
-      -v $(dirname $(readlink --canonicalize ${0}))/scripts/docker.tar.bz2:/etc/docker.tar.bz2 \
-      daltonmatos/mesos:0.0.3
-done
-
-for SLAVE_IP in `echo ${MESOS_SLAVE_IPS_ACCOUNT_ASGARD_DEV}`;
-do
-    echo -n "Mesos Slave (namespace=asgard-dev (${SLAVE_IP}) "
-    docker run -d --rm -it --ip ${SLAVE_IP} \
-      --name asgard_mesosslave_${RANDOM} \
-      --net ${NETWORK_NAME} \
-      --env-file <(
-    echo MESOS_IP=${SLAVE_IP}
-    echo LIBPROCESS_ADVERTISE_IP=${SLAVE_IP}
-    echo MESOS_ATTRIBUTES="${MESOS_ATTRIBUTES};owner:asgard-dev"
-    echo MESOS_MASTER=${MESOS_MASTER}
-    echo MESOS_RESOURCES=${MESOS_RESOURCES}
-    ) \
-      -v /sys/fs/cgroup:/sys/fs/cgroup \
-      -v /var/run/docker.sock:/var/run/docker.sock \
-      -v $(dirname $(readlink --canonicalize ${0}))/scripts/docker.tar.bz2:/etc/docker.tar.bz2 \
-      daltonmatos/mesos:0.0.3
-done
+source "dev/mesosslave.sh"
 
 #CREATE INITIAL GROUP
 echo "Creating initial groups. Waiting for Marathon to come up..."
