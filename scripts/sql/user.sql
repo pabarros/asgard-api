b9edf33eceac30a3018c1fb088a093a0e293f296
Dalton Barreto <daltonmatos@gmail.com>
2018-08-21 18:07:35 -0300

    Criando account para Asgard Core

--- scripts/sql/user.sql
+++ scripts/sql/user.sql
@@ -2,10 +2,10 @@
 
 insert into account (name, namespace, owner) values ('Asgard/DEV', 'asgard-dev', 'asgard-dev');
 insert into account (name, namespace, owner) values ('Asgard/INFRA', 'asgard-infra', 'asgard-infra');
+insert into account (name, namespace, owner) values ('Asgard/Core', 'asgard', 'asgard');
 
-insert into "user" (tx_name, tx_email, tx_authkey, bl_system) values ('Rafael Amorim', 'rafael@admatic.com.br', 'cccccchcdhjrtrgbtndbedljrectjhv', false);
 INSERT INTO "user" (tx_name, tx_email, tx_authkey, bl_system) VALUES ('Dalton Barreto', 'daltonmatos@gmail.com', '15ce068d5d2942b18a7f8ae76933f53', false);
-
+INSERT into "user" (tx_name, tx_email, tx_authkey, bl_system) values ('Rafael Amorim', 'rafael@admatic.com.br', 'cccccchcdhjrtrgbtndbedljrectjhv', false);
 INSERT INTO "user" (tx_name, tx_email, tx_authkey, bl_system) VALUES ('Danilo Pereira', 'danilo.nascimento@b2wdigital.com', 'd8c336eb8cca4d39a8425a151ac84f81', false);
 
 INSERT INTO user_has_account (account_id, user_id) VALUES (
@@ -15,6 +15,11 @@ INSERT INTO user_has_account (account_id, user_id) VALUES (
 
 INSERT INTO user_has_account (account_id, user_id) VALUES (
   (SELECT id from account where name = 'Asgard/INFRA'),
+  (SELECT id from "user" where tx_email='daltonmatos@gmail.com')
+);
+
+INSERT INTO user_has_account (account_id, user_id) VALUES (
+  (SELECT id from account where name = 'Asgard/Core'),
   (SELECT id from "user" where tx_email='daltonmatos@gmail.com')
 );
 
