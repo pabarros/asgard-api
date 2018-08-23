9d9890c89b5dd70545f1154f6f40e0f19c7b1208
Dalton Barreto <daltonmatos@gmail.com>
2018-08-23 13:11:22 -0300

    Refatorando script que cria as apps
    IN-2634

--- dev/create-apps.sh
+++ dev/create-apps.sh
@@ -4,30 +4,33 @@ source "dev/vars.sh"
 BASEDIR=$(dirname $(readlink --canonicalize ${0}))
 
 
+function put_json(){
+  json=${1}
+  deployment=$(curl -s -H "Content-type: application/json" -X PUT -d@${json} http://${MARATHON_IP}:8080/v2/apps)
+  echo "${json} ${deployment}"
+}
+
 echo ""
 echo "Creating initial apps...${BASEDIR}"
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/redis.json http://${MARATHON_IP}:8080/v2/apps
+put_json "${BASEDIR}/dev/apps/asgard/redis.json"
 
 # MySQL 172.18.70.10
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard-dev/mysql.json http://${MARATHON_IP}:8080/v2/apps
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard-dev/phpmyadmin.json http://${MARATHON_IP}:8080/v2/apps
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard-dev/wordpress.json http://${MARATHON_IP}:8080/v2/apps
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard-dev/httpbin.json http://${MARATHON_IP}:8080/v2/apps
+put_json "${BASEDIR}/dev/apps/asgard-dev/mysql.json"
+put_json "${BASEDIR}/dev/apps/asgard-dev/phpmyadmin.json"
+put_json "${BASEDIR}/dev/apps/asgard-dev/wordpress.json"
+put_json "${BASEDIR}/dev/apps/asgard-dev/httpbin.json"
 
 # 172.18.80.1
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/rabbitmq.json http://${MARATHON_IP}:8080/v2/apps
+put_json "${BASEDIR}/dev/apps/asgard/rabbitmq.json"
 
 # 172.18.70.1
-#curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/elasticsearch.json http://${MARATHON_IP}:8080/v2/apps
 docker run --rm --net=asgard --ip 172.18.70.1 --name "asgard_elasticsearch_$$" -d elasticsearch:5.5-alpine
 
 # 172.18.70.2
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/fluentd.json http://${MARATHON_IP}:8080/v2/apps
+put_json "${BASEDIR}/dev/apps/asgard/fluentd.json"
 
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/log-indexer.json http://${MARATHON_IP}:8080/v2/apps
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/kibana.json http://${MARATHON_IP}:8080/v2/apps
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard/stats-collector.json http://${MARATHON_IP}:8080/v2/apps
+put_json "${BASEDIR}/dev/apps/asgard/log-indexer.json"
+put_json "${BASEDIR}/dev/apps/asgard/kibana.json"
+put_json "${BASEDIR}/dev/apps/asgard/stats-collector.json"
 
-
-curl -s -H "Content-type: application/json" -X PUT -d@${BASEDIR}/dev/apps/asgard-dev/echo.json http://${MARATHON_IP}:8080/v2/apps
-
+put_json "${BASEDIR}/dev/apps/asgard-dev/echo.json"
