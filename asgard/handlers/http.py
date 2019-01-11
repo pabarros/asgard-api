b72d0be16df628d7e701445c09e6b18c97f3d9ab
Dalton Barreto <daltonmatos@gmail.com>
2019-01-11 11:12:24 -0200

    Dividindo onde ficarão os endpoints da API

--- asgard/handlers/http.py
+++ asgard/handlers/http.py
@@ -2,7 +2,5 @@ from asyncworker import RouteTypes
 from asgard.app import app
 from aiohttp import web
 
-
-@app.route(["/"], methods=["GET"], type=RouteTypes.HTTP)
-async def handle(r):
-    return web.json_response({"OK": True})
+from asgard.api import agents
+from asgard.api import index
