d83676685feb2d49a041d5d14faa963dec48b857
Dalton Barreto <daltonmatos@gmail.com>
2019-01-14 14:07:38 -0200

    Teste para App() pegar os dados da config certa.

--- asgard/app.py
+++ asgard/app.py
@@ -1,4 +1,4 @@
 from asyncworker import App
+from asgard import conf
 
-app = App("", "", "", 1024)
-
+app = App(host=conf.ASGARD_RABBITMQ_HOST, user=conf.ASGARD_RABBITMQ_USER, password=conf.ASGARD_RABBITMQ_PASS, prefetch_count=conf.ASGARD_RABBITMQ_PREFETCH)
