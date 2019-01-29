7b428447c829fff6196de71a37c2b1bde5e2d89e
Dalton Barreto <daltonmatos@gmail.com>
2019-01-29 15:52:40 -0200

    Implementação completa do primeiro endpoint async
    Com autenticação e já filtrando os resultados de acordo com o
    namespace do usuário logado

--- asgard/api/agents.py
+++ asgard/api/agents.py
@@ -4,9 +4,14 @@ from aiohttp import web
 
 from asgard.services import agents_service
 from asgard.backends import mesos
+from asgard.http.auth import auth_required
 
 
 @app.route(["/agents"], type=RouteTypes.HTTP, methods=["GET"])
+@auth_required
 async def agents_handler(request: web.Request):
-    agents_info = await agents_service.get_agents(namespace="", backend=mesos)
+    namespace = request["user"].current_account.namespace
+    agents_info = await agents_service.get_agents(
+        namespace=namespace, backend=mesos
+    )
     return web.json_response(agents_info)
