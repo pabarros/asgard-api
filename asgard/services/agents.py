c4c0805930835310bda17e392f0de4043c1de7ba
Dalton Barreto <daltonmatos@gmail.com>
2019-02-13 12:37:53 -0200

    Ajustando typehint

--- asgard/services/agents.py
+++ asgard/services/agents.py
@@ -1,20 +1,16 @@
 from typing import List
 
 
+from asgard.backends.base import Backend
 from asgard.services.models.agent import Agent
 from asgard.services.models.app import App
 
 
 class AgentsService:
-    async def get_agents(
-        self, namespace: str, backend: "asgard.backends.base.Backend"
-    ) -> List[Agent]:
+    async def get_agents(self, namespace: str, backend: Backend) -> List[Agent]:
         return await backend.get_agents(namespace)
 
     async def get_apps(
-        self,
-        namespace: str,
-        agent_id: str,
-        backend: "asgard.backends.base.Backend",
+        self, namespace: str, agent_id: str, backend: Backend
     ) -> List[App]:
         return await backend.get_apps(namespace, agent_id)
