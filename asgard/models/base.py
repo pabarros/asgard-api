659864bd2363c5186a2bde98f738f35d127d6058
Dalton Barreto <daltonmatos@gmail.com>
2019-02-23 23:31:38 -0300

    Primeira implementação de um ModelFactory genérico
    Idealmente todos os modelos deverão ter um tipo. Para cada modelo usado
    peleo asgard (App, Task, Pod, Agent, etc) teremos um tipo para cada
    backend: {Mesos, Kube, ...}App, {Mesos, Kube, ...}App e etc.

--- asgard/models/base.py
+++ asgard/models/base.py
@@ -1,9 +1,25 @@
-from typing import Dict
-from pydantic import BaseModel
+from typing import Dict, Type
+from pydantic import BaseModel as PydanticBaseModel
 
 
-class Model(BaseModel):
+class Model(PydanticBaseModel):
     errors: Dict[str, str] = {}
 
     def add_error(self, field_name, error_msg):
         self.errors[field_name] = error_msg
+
+
+class BaseModel(PydanticBaseModel):
+    type: str
+
+
+def ModelFactory(subclass_marker):
+    class _ModelFactory:
+        def __new__(cls, *args, **kwargs):
+            type_ = kwargs.pop("type")
+            for subclass in subclass_marker.__subclasses__():
+                if subclass.__fields__["type"].default == type_:
+                    return subclass(*args, **kwargs)
+            raise ValueError(f"'{type_}' is an invalid {subclass_marker} type. ")
+
+    return _ModelFactory
---
+++ tests/asgard/models/test_base_model.py
@@ -0,0 +1,22 @@
+from asynctest import TestCase
+
+from asgard.models.base import ModelFactory, BaseModel
+
+
+class BaseModelTest(TestCase):
+    async def test_choose_instance_based_on_type_field(self):
+        class BaseAgent(BaseModel):
+            pass
+
+        Agent = ModelFactory(BaseAgent)
+
+        class ModelOne(BaseAgent):
+            type: str = "ONE"
+            other_field: str
+
+        class ModelTwo(BaseAgent):
+            type: str = "TWO"
+
+        instance = Agent(**{"type": "ONE", "other_field": "value"})
+        self.assertIsInstance(instance, ModelOne)
+        self.assertDictEqual({"type": "ONE", "other_field": "value"}, instance.dict())
